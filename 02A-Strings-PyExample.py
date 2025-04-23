92d6f450f57f506b21b050ed895b8792137ff6bf
NightShade8 <andrewih2003@gmail.com>
2025-04-23 11:27:21 -0700

    Understanding Strings in Python with Examples

---
+++ 02A-Strings-PyExample.py
@@ -0,0 +1,5 @@
+print ("Hello World!")
+print ('Hello World!')
+print ("""This string runs
+       multiple line""") #Triple Quotes for multi-line strings
+print ("This is a " + "string!") #Cencatenating strings
